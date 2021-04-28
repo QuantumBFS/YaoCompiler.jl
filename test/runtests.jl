08470e9ec5b6049cb97819e9815fbdbb1239ff67
Roger-Luo <rogerluo.rl18@gmail.com>
2021-04-28 00:07:45 -0400

    add test back

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,9 @@
 using YaoCompiler
 using Test
 
-# @testset "interpreter" begin
-#     include("interp.jl")    
-# end
+@testset "interpreter" begin
+    include("interp.jl")    
+end
 
 @testset "emulate" begin
     include("emulate.jl")
