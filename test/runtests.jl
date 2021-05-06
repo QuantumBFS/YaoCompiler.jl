13832a2d4545a122bff6400788b08c228beaee6e
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-06 00:14:42 -0400

    add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,14 @@ using Test
 @device function basic()
     1 => X
     2 => Y
+    3 => Z
+    4 => H
+    4 => T
+    4 => S
+    4 => Rx(1.2)
+    4 => Ry(1.2)
+    4 => Rz(1.2)
+    4 => shift(1.2)
 end
 
 @testset "basic" begin
@@ -17,13 +25,13 @@ end
     @test measure_ssa_uses(ci) == Set{Int}()
     mi = MemoryInfo(ci)
     display(mi)
-    @test mi.qubits == Dict(1=>0, 2=>1)
-    @test mi.n_qubits == 2
+    @test mi.qubits == Dict(1=>0, 2=>1, 3=>2, 4=>3)
+    @test mi.n_qubits == 4
 
     @test code_qobj(typeof(basic())) == Experiment(;
         header = Dict{String, Any}(
             "description" => "Operation{typeof(basic), Tuple{}}",
-            "n_qubits" => 2,
+            "n_qubits" => 4,
         ),
         config = ExpConfig(;
             memory_slots = 0,
@@ -38,6 +46,42 @@ end
             Gate(;
                 name = "y",
                 qubits = [1],
+            ),
+            Gate(;
+                name = "z",
+                qubits = [2],
+            ),
+            Gate(;
+                name = "h",
+                qubits = [3],
+            ),
+            Gate(;
+                name = "t",
+                qubits = [3],
+            ),
+            Gate(;
+                name = "s",
+                qubits = [3],
+            ),
+            Gate(;
+                name = "rx",
+                qubits = [3],
+                params = [1.2],
+            ),
+            Gate(;
+                name = "ry",
+                qubits = [3],
+                params = [1.2],
+            ),
+            Gate(;
+                name = "rz",
+                qubits = [3],
+                params = [1.2],
+            ),
+            Gate(;
+                name = "u1",
+                qubits = [3],
+                params = [1.2],
             ),
         ],
     )
@@ -354,6 +398,129 @@ end
                 name = "cy",
                 qubits = [0, 2],
                 conditional = 0,
+            ),
+        ],
+    )
+end
+
+@device function test_barrier()
+    1 => H
+    3 => X
+    @barrier 1,3
+    c = @measure 1
+    if c == 1
+        @ctrl 2 1 => Y
+    end
+    return
+end
+
+@testset "test_barrier" begin
+    @test code_qobj(typeof(test_barrier())) == Experiment(;
+        header = Dict{String, Any}(
+            "description" => "Operation{typeof(test_barrier), Tuple{}}",
+            "n_qubits" => 3,
+        ),
+        config = ExpConfig(;
+            memory_slots = 1,
+            seed = 1,
+            max_credits = 3,
+        ),
+        instructions = [
+            Gate(;
+                name = "h",
+                qubits = [0],
+            ),
+            Gate(;
+                name = "x",
+                qubits = [1],
+            ),
+            Barrier(;
+                qubits = [0, 1],
+            ),
+            Measure(;
+                qubits = [0],
+                memory = [0],
+                register = [0],
+            ),
+            Gate(;
+                name = "cy",
+                qubits = [0, 2],
+                conditional = 0,
+            ),
+        ],
+    )
+end
+
+@device function test_ctrl()
+    @ctrl 4 1 => H
+    @ctrl 4 3 => X
+    @ctrl 4 3 => Y
+    @ctrl 4 3 => Z
+    @ctrl (4,5) 3 => X
+    @ctrl 4 3 => Rx(2.1)
+    @ctrl 4 3 => Ry(2.1)
+    @ctrl 4 3 => Rz(2.1)
+    @ctrl 4 3 => shift(2.1)
+    @ctrl 4 3 => Intrinsics.UGate(1.2, 2.2, 3.3)
+    return
+end
+
+@testset "test_ctrl" begin
+    @test code_qobj(typeof(test_ctrl())) == Experiment(;
+        header = Dict{String, Any}(
+            "description" => "Operation{typeof(test_ctrl), Tuple{}}",
+            "n_qubits" => 4,
+        ),
+        config = ExpConfig(;
+            memory_slots = 0,
+            seed = 1,
+            max_credits = 3,
+        ),
+        instructions = [
+            Gate(;
+                name = "ch",
+                qubits = [0, 1],
+            ),
+            Gate(;
+                name = "cx",
+                qubits = [2, 1],
+            ),
+            Gate(;
+                name = "cy",
+                qubits = [2, 1],
+            ),
+            Gate(;
+                name = "cz",
+                qubits = [2, 1],
+            ),
+            Gate(;
+                name = "ccx",
+                qubits = [2, 1, 3],
+            ),
+            Gate(;
+                name = "crx",
+                qubits = [2, 1],
+                params = [2.1],
+            ),
+            Gate(;
+                name = "cry",
+                qubits = [2, 1],
+                params = [2.1],
+            ),
+            Gate(;
+                name = "crz",
+                qubits = [2, 1],
+                params = [2.1],
+            ),
+            Gate(;
+                name = "cu1",
+                qubits = [2, 1],
+                params = [2.1],
+            ),
+            Gate(;
+                name = "cu3",
+                qubits = [2, 1],
+                params = [1.2, 2.2, 3.3],
             ),
         ],
     )
